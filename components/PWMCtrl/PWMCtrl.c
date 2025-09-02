fe13160c9764b1c22748ff92fe913ac0c29eaaa3
Xuanzheng0901 <szx1691196289@gmail.com>
2025-09-03 05:39:40 +0800

    优化代码结构

--- components/PWMCtrl/PWMCtrl.c
+++ components/PWMCtrl/PWMCtrl.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 #include "PWMCtrl.h"
 
-void set_pwm_duty(int32_t duty)
+void set_pwm_duty(uint8_t channel, int32_t duty)
 {
-    ledc_set_duty(LEDC_LOW_SPEED_MODE, 1, duty);
-    ledc_update_duty(LEDC_LOW_SPEED_MODE, 1);
+    ledc_set_duty(LEDC_LOW_SPEED_MODE, channel, duty);
+    ledc_update_duty(LEDC_LOW_SPEED_MODE, channel);
 }
 
 void pwm_init(void)
 {
     ledc_timer_config_t timer_config = {
         .clk_cfg = LEDC_AUTO_CLK,
-        .duty_resolution = LEDC_TIMER_7_BIT,
+        .duty_resolution = LEDC_TIMER_11_BIT,
         .freq_hz = 20000,
         .speed_mode = LEDC_LOW_SPEED_MODE,
         .timer_num = LEDC_TIMER_0,
@@ -22,16 +22,13 @@ void pwm_init(void)
         .duty = 0,
         .flags.output_invert = 0,
         .speed_mode = LEDC_LOW_SPEED_MODE,
-        .timer_sel = LEDC_TIMER_0};
-
-    channel_config.channel = 1;
-    channel_config.gpio_num = 1;
-    channel_config.duty = 80;
-
+        .timer_sel = LEDC_TIMER_0,
+        .channel = 1,
+        .gpio_num = VSET_IO
+    };
     ledc_channel_config(&channel_config);
 
     channel_config.channel = 2;
-    channel_config.gpio_num = 2;
-    channel_config.duty = 90;
+    channel_config.gpio_num = ISET_IO;
     ledc_channel_config(&channel_config);
 }
--- components/PWMCtrl/include/PWMCtrl.h
+++ components/PWMCtrl/include/PWMCtrl.h
@@ -4,7 +4,13 @@
 #include "stdio.h"
 #include "driver/ledc.h"
 
+#define VSET_IO 9
+#define ISET_IO 10
+
+#define VSET_CHN 0
+#define ISET_CHN 1
+
 void pwm_init(void);
-void set_pwm_duty(int32_t duty);
+void set_pwm_duty(uint8_t channel, int32_t duty);
 
 #endif
