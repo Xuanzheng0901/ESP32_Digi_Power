1d4736fed0f415354baa687567d9154828fc453c
Xuanzheng0901 <szx1691196289@gmail.com>
2025-09-03 05:34:18 +0800

    优化代码结构

--- components/ADCDetect/ADCDetect.c
+++ components/ADCDetect/ADCDetect.c
@@ -4,10 +4,11 @@
 
 static const char *TAG = "ADS1115";
 
-i2c_master_bus_handle_t bus_handle;
-i2c_master_dev_handle_t dev_handle;
+static i2c_master_bus_handle_t bus_handle;
+static i2c_master_dev_handle_t dev_handle;
+static volatile int16_t ADS1115_raw[2];
 
-void ADS1115_write(uint8_t addr, uint16_t data)
+static void ADS1115_write(uint8_t addr, uint16_t data)
 {
     uint8_t buffer[3] = {0};
     buffer[0] = addr;
@@ -45,39 +46,22 @@ void ADS1115_read_channel(uint8_t channel, int16_t* value)
 
 void ADS1115_Init(void)
 {
-    esp_err_t ret;
-    
-    i2c_master_bus_config_t bus_cfg = 
-    {
+    i2c_master_bus_config_t bus_cfg = {
         .clk_source                   = I2C_CLK_SRC_DEFAULT,
         .flags.enable_internal_pullup = true,
         .i2c_port                     = I2C_NUM_0, 
-        .scl_io_num                   = 10,
-        .sda_io_num                   = 11,
+        .scl_io_num                   = ADS1115_SCL,
+        .sda_io_num                   = ADS1115_SDA,
         .trans_queue_depth            = 16
     };
-    ret = i2c_new_master_bus(&bus_cfg, &bus_handle);
-    if (ret != ESP_OK) {
-        printf("Failed to create I2C master bus: %s\n", esp_err_to_name(ret));
-        return;
-    }
+    i2c_new_master_bus(&bus_cfg, &bus_handle);
     
-    i2c_device_config_t ads1115_cfg = 
-    {
+    i2c_device_config_t ads1115_cfg = {
         .dev_addr_length         = I2C_ADDR_BIT_LEN_7, 
         .device_address          = 0x48,
         .flags.disable_ack_check = false,
         .scl_speed_hz            = 400 * 1000
     };
-    ret = i2c_master_bus_add_device(bus_handle, &ads1115_cfg, &dev_handle);
-    if (ret != ESP_OK) {
-        printf("Failed to add I2C device: %s\n", esp_err_to_name(ret));
-        return;
-    }
-
-    uint16_t config = ADS1115_CONFIG_DEFAULT;
-    uint8_t buffer[2] = {0};
-    i2c_master_transmit_receive(dev_handle, (uint8_t*)&config, 2, buffer, 2, 10);
-    vTaskDelay(pdMS_TO_TICKS(100));
-    ADS1115_write(0x01, config);
+    i2c_master_bus_add_device(bus_handle, &ads1115_cfg, &dev_handle);
+    xTaskCreate(ADS1115_read_task, "ADS1115", 4096, NULL, 5, NULL);
 }
